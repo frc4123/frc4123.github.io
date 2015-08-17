be1056978153a071ca71aa494f94c050f1fc5ce7
Adrian Campos <dev.adriancampos@live.com>
2015-08-16 21:34:56 -0700

    Updated "Our Works/Gallery"

--- js/portfolio_slider.js
+++ js/portfolio_slider.js
@@ -5,7 +5,47 @@ $("#elastic_grid_demo").elastic_grid({
 	'hoverInverse': false,
 	'expandingSpeed': 500,
 	'expandingHeight': 500,
-	'items' :
+    items:
+    [
+			{
+			'title' : '2012',
+			'description'   : '',
+			'thumbnail' : [''],
+			'large' : [''],
+			'button_list'   :
+			[
+//			{ 'title':'Demo', 'url' : 'http://#' },
+//			{ 'title':'Download', 'url':'http://#'}
+			],
+			'tags'  : []
+			},
+
+			{
+			'title' : '2013',
+			'description'   : '',
+			'thumbnail' : ['img/portfolio/small/1.jpg', 'img/portfolio/small/3.jpg', 'img/portfolio/small/4.jpg', 'img/portfolio/small/6.jpg', 'img/portfolio/small/7.jpg', 'img/portfolio/small/8.jpg', 'img/portfolio/small/15.jpg', 'img/portfolio/small/16.jpg', 'img/portfolio/small/17.jpg', 'img/portfolio/small/18.jpg', 'img/portfolio/small/19.jpg', 'img/portfolio/small/20.jpg'],
+			'large' : ['img/portfolio/large/1.jpg', 'img/portfolio/large/3.jpg', 'img/portfolio/large/4.jpg', 'img/portfolio/large/6.jpg', 'img/portfolio/large/7.jpg', 'img/portfolio/large/8.jpg', 'img/portfolio/large/15.jpg', 'img/portfolio/large/16.jpg', 'img/portfolio/large/17.jpg', 'img/portfolio/large/18.jpg', 'img/portfolio/large/19.jpg', 'img/portfolio/large/20.jpg'],
+			'button_list'   : [],
+			'tags'  : []
+			},
+        {
+			'title' : '2014',
+			'description'   : '',
+			'thumbnail' : ['img/portfolio/small/2.jpg', 'img/portfolio/small/5.jpg', 'img/portfolio/small/9.jpg', 'img/portfolio/small/10.jpg', 'img/portfolio/small/11.jpg', 'img/portfolio/small/12.jpg', 'img/portfolio/small/13.jpg', 'img/portfolio/small/14.jpg'],
+			'large' : ['img/portfolio/large/2.jpg', 'img/portfolio/large/5.jpg', 'img/portfolio/large/9.jpg', 'img/portfolio/large/10.jpg', 'img/portfolio/large/11.jpg', 'img/portfolio/large/12.jpg', 'img/portfolio/large/13.jpg', 'img/portfolio/large/14.jpg'],
+			'button_list'   : [],
+			'tags'  : []
+			},
+        {
+			'title' : '2015',
+			'description'   : '',
+			'thumbnail' : [''],
+			'large' : [''],
+			'button_list'   : [],
+			'tags'  : []
+			},
+        ]
+	/*'items' :
 		[
 			{
 			'title' : 'Gallery 1',
@@ -202,6 +242,6 @@ $("#elastic_grid_demo").elastic_grid({
 			'tags'  : ['Jquery', 'Design']
 			}
 	 
-		]
+		] */
 	});
 });
